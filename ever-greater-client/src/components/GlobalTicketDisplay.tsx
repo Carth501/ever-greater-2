8a99bfdade69caf32fc16d7c3fdc971e35080308
carth501 <carth501@gmail.com>
2026-02-22 13:46:28 -0500

    refactor: simplify GlobalTicketDisplay component by removing scientific notation option

--- ever-greater-client/src/components/GlobalTicketDisplay.tsx
+++ ever-greater-client/src/components/GlobalTicketDisplay.tsx
@@ -3,20 +3,13 @@ import "./GlobalTicketDisplay.css";
 
 interface Props {
   scalingNumber: number;
-  scientific?: boolean;
 }
 
-function GlobalTicketDisplay({
-  scalingNumber,
-  scientific = true,
-}: Props): JSX.Element {
+function GlobalTicketDisplay({ scalingNumber }: Props): JSX.Element {
   return (
     <div className="box global-ticket-display">
       <h1>
-        <strong>Ticket Pool:</strong>{" "}
-        {scientific
-          ? scalingNumber.toExponential(2)
-          : scalingNumber.toLocaleString()}
+        <strong>Ticket Pool:</strong> {scalingNumber.toLocaleString()}
       </h1>
     </div>
   );
