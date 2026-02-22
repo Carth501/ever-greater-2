ad0c8647a6cc515992758aec7e0f78d3fb38938f
carth501 <carth501@gmail.com>
2026-02-22 13:33:29 -0500

    feat: add footer with designer logo and corresponding styles

--- ever-greater-client/src/App.tsx
+++ ever-greater-client/src/App.tsx
@@ -1,10 +1,15 @@
 import "./App.css";
 import ScalingNumberDemo from "./components/ScalingNumberDemo";
+import cLogo from "./images/cLogo.png";
 
 function App() {
   return (
     <div className="App">
       <ScalingNumberDemo />
+
+      <footer className="App-footer">
+        <img src={cLogo} alt="site by C" className="designer-logo" />
+      </footer>
     </div>
   );
 }
